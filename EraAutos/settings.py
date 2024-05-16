e1eada3988e21a4c74fdc43d59717e699a5174d2
Adonis <adonis.casado@outlook.com>
2024-05-15 21:08:08 -0400

    update

--- EraAutos/settings.py
+++ EraAutos/settings.py
@@ -136,7 +136,7 @@ DATABASES = {
       #  'PASSWORD': 'password',
       #  'HOST': 'localhost',
   #  }
-}
+#}
 
 
 # Password validation
@@ -174,10 +174,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 #Media Files
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
